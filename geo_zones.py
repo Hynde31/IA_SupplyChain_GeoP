5ae1203774b10dbcdc362bc1a340a7d4ac5e2376
Hynde31 <hynde313@gmail.com>
2025-06-30 16:22:16 +0200

    Update geo_zones.py

--- geo_zones.py
+++ geo_zones.py
@@ -1,45 +1,43 @@
-# -*- coding: utf-8 -*-
-# Définition des zones à risque géopolitiques (manuel)
-
-ZONES_GEO = [
+    # Zone à risque géopolitique (tension majeure)
     {
-        "type": "Zone à risque",
+        "type": "Zone à risque géopolitique",
+        "label": "Mer Rouge",
+        "latitude": 15.0,
+        "longitude": 42.0,
+        "Pays": "Mer Rouge",
+        "Couleur": [255, 215, 0],  # Jaune (tension majeure)
+        "Impact": "Tensions maritimes",
+        "Criticité": "Moyenne",
+        "Site": "-",
+        "MRP Code": "-",
+        "Désignation": "-",
+    },
+    # Zone conflit armé
+    {
+        "type": "Zone conflit armé",
         "label": "Israël/Gaza",
         "latitude": 31.5,
         "longitude": 34.8,
         "Pays": "Israël",
-        "Couleur": [220, 30, 30],
+        "Couleur": [220, 30, 30],  # Rouge
         "Impact": "Conflit armé",
-        "Criticité": "",
-        "Site": "",
-        "MRP Code": "",
-        "Désignation": "",
+        "Criticité": "Élevée",
+        "Site": "-",
+        "MRP Code": "-",
+        "Désignation": "-",
     },
     {
-        "type": "Zone à risque",
+        "type": "Zone conflit armé",
         "label": "Ukraine",
         "latitude": 49.0,
         "longitude": 32.0,
         "Pays": "Ukraine",
         "Couleur": [220, 30, 30],
         "Impact": "Conflit armé",
-        "Criticité": "",
-        "Site": "",
-        "MRP Code": "",
-        "Désignation": "",
+        "Criticité": "Élevée",
+        "Site": "-",
+        "MRP Code": "-",
+        "Désignation": "-",
     },
-    {
-        "type": "Zone à risque",
-        "label": "Mer Rouge",
-        "latitude": 15.0,
-        "longitude": 42.0,
-        "Pays": "Mer Rouge",
-        "Couleur": [255, 140, 0],
-        "Impact": "Tensions maritimes",
-        "Criticité": "",
-        "Site": "",
-        "MRP Code": "",
-        "Désignation": "",
-    },
-    # Ajoute ici d'autres zones si besoin
+    # Ajoute d'autres zones si besoin...
 ]
