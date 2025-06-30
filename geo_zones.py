26c16ced96af20b7e8bb4691efb59d0a857998fc
Hynde31 <hynde313@gmail.com>
2025-06-30 16:16:20 +0200

    Create geo_zones.py

---
+++ geo_zones.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Définition des zones à risque géopolitiques (manuel)
+
+ZONES_GEO = [
+    {
+        "type": "Zone à risque",
+        "label": "Israël/Gaza",
+        "latitude": 31.5,
+        "longitude": 34.8,
+        "Pays": "Israël",
+        "Couleur": [220, 30, 30],
+        "Impact": "Conflit armé",
+        "Criticité": "",
+        "Site": "",
+        "MRP Code": "",
+        "Désignation": "",
+    },
+    {
+        "type": "Zone à risque",
+        "label": "Ukraine",
+        "latitude": 49.0,
+        "longitude": 32.0,
+        "Pays": "Ukraine",
+        "Couleur": [220, 30, 30],
+        "Impact": "Conflit armé",
+        "Criticité": "",
+        "Site": "",
+        "MRP Code": "",
+        "Désignation": "",
+    },
+    {
+        "type": "Zone à risque",
+        "label": "Mer Rouge",
+        "latitude": 15.0,
+        "longitude": 42.0,
+        "Pays": "Mer Rouge",
+        "Couleur": [255, 140, 0],
+        "Impact": "Tensions maritimes",
+        "Criticité": "",
+        "Site": "",
+        "MRP Code": "",
+        "Désignation": "",
+    },
+    # Ajoute ici d'autres zones si besoin
+]
