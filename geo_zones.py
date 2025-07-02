d942d0dde2b417ae50cd77c6291e5ff518887708
Hynde31 <hynde313@gmail.com>
2025-07-02 11:13:28 +0200

    Update geo_zones.py

--- geo_zones.py
+++ geo_zones.py
@@ -1,5 +1,26 @@
 ZONES_GEO = [
-    {"Nom": "Russie", "Description": "Zone sous sanctions", "Impact": 0.5, "Latitude": 56.0, "Longitude": 38.0, "type": "Zone", "Couleur": [255, 0, 0]},
-    {"Nom": "Chine", "Description": "Zone sous tension commerciale", "Impact": 0.3, "Latitude": 35.0, "Longitude": 105.0, "type": "Zone", "Couleur": [255, 165, 0]},
-    {"Nom": "Moyen-Orient", "Description": "Instabilités régionales", "Impact": 0.4, "Latitude": 30.0, "Longitude": 45.0, "type": "Zone", "Couleur": [255, 165, 0]}
+    {
+        "Nom": "Russie",
+        "Description": "Zone sous sanctions",
+        "Impact": 0.5,
+        "Latitude": 56.0,
+        "Longitude": 38.0,
+        "Couleur": [255, 0, 0]
+    },
+    {
+        "Nom": "Chine",
+        "Description": "Zone sous tension commerciale",
+        "Impact": 0.3,
+        "Latitude": 35.0,
+        "Longitude": 105.0,
+        "Couleur": [255, 165, 0]
+    },
+    {
+        "Nom": "Moyen-Orient",
+        "Description": "Instabilités régionales",
+        "Impact": 0.4,
+        "Latitude": 30.0,
+        "Longitude": 45.0,
+        "Couleur": [255, 165, 0]
+    }
 ]
