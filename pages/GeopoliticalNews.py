72522ee7e814ed9c7b099f815874b8c1c035ca2a
Hynde31 <hynde313@gmail.com>
2025-10-21 16:26:24 +0200

    Update GeopoliticalNews.py

--- pages/GeopoliticalNews.py
+++ pages/GeopoliticalNews.py
@@ -7,9 +7,10 @@ st.title("📰 Synthèse des zones géopolitiques")
 df_geo = pd.DataFrame(ZONES_GEO)
 
 # Vérifier colonnes existantes et les adapter
-cols_to_show = [col for col in ["Nom", "Description", "Impact", "Latitude", "Longitude", "Couleur"] if col in df_geo.columns]
+cols_to_show = [col for col in ["Nom", "Description", "Impact"] if col in df_geo.columns]
 
 if cols_to_show:
     st.dataframe(df_geo[cols_to_show], use_container_width=True)
 else:
     st.warning("Structure des données zones géopolitiques incorrecte.")
+
